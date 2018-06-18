fc5858cc0a31477a5228e41d6490c2e0df57dff9
rmesterca <ralucamesterca@gmail.com>
2018-06-18 15:27:40 +0300

    simpleApp_NavigationControllerAdventure added

---
+++ simpleApps/simpleApp_NavigationControllerAdventure/simpleApp_NavigationControllerAdventure/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  simpleApp_NavigationControllerAdventure
+//
+//  Created by Mac on 18/06/2018.
+//  Copyright © 2018 Fig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ViewController.startOver))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    @objc func startOver() {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
+
+}
+
