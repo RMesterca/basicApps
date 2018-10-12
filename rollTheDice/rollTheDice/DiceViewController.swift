60e203f541b1e4702da159d8895a441c10900137
rmesterca <ralucamesterca@gmail.com>
2018-10-12 15:42:47 +0200

    Roll the Dice code refactoring

--- rollTheDice/rollTheDice/DiceViewController.swift
+++ rollTheDice/rollTheDice/DiceViewController.swift
@@ -9,58 +9,53 @@
 import Foundation
 import UIKit
 
-// MARK: - DiceViewController: UIViewController
-
 class DiceViewController: UIViewController {
     
     // MARK: Properties
-    
     var firstValue: Int?
     var secondValue: Int?
     
     // MARK: Outlets
-    
     @IBOutlet weak var firstDie: UIImageView!
     @IBOutlet weak var secondDie: UIImageView!
     
     // MARK: Life Cycle
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     override func viewWillAppear(_ animated: Bool) {
-        
-        // The dice will only appear if firstValue and secondValue have been set
-        if let firstValue = self.firstValue {
-            self.firstDie.image = UIImage(named: "d\(firstValue)")
-        } else {
-            self.firstDie.image = nil;
-        }
-        
-        if let secondValue = self.secondValue {
-            self.secondDie.image = UIImage(named: "d\(secondValue)")
-        } else {
-            self.secondDie.image = nil
-        }
-        
-        self.firstDie.alpha = 0
-        self.secondDie.alpha = 0
+        super.viewWillAppear(true)
+        generateDiceValue()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        
         UIView.animate(withDuration: 0.3) {
-            self.firstDie.alpha = 1
-            self.secondDie.alpha = 1
+            self.setTransparency(alpha: 1)
         }
     }
     
-    // MARK: Generate Image for Dice
-    
-    func imageForValue(_ value: Int?) -> UIImage? {
-        return nil
-    }
-    
     // MARK: Actions
-    
     @IBAction func dismiss() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: Methods
+    func generateDiceValue () {
+        guard let firstValue = self.firstValue else {
+            return
+        }
+        
+        guard let secondValue = self.secondValue else {
+            return
+        }
+        
+        self.firstDie.image = UIImage(named: "d\(firstValue)")
+        self.secondDie.image = UIImage(named: "d\(secondValue)")
+        setTransparency(alpha: 0)
+    }
+    
+    func setTransparency(alpha: CGFloat) {
+        self.firstDie.alpha = alpha
+        self.secondDie.alpha = alpha
     }
 }
--- rollTheDice/rollTheDice/RollViewController.swift
+++ rollTheDice/rollTheDice/RollViewController.swift
@@ -8,30 +8,21 @@
 
 import UIKit
 
-// MARK: - RollViewController: UIViewController
-
 class RollViewController: UIViewController {
-    
-    // MARK: Segue
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "rollDice" {
-            let controller = segue.destination as! DiceViewController
+            guard let controller = segue.destination as? DiceViewController else {
+                return
+            }
             
             controller.firstValue = self.randomDiceValue()
             controller.secondValue = self.randomDiceValue()
         }
     }
     
-    // MARK: Generate Dice Value
-    
     func randomDiceValue() -> Int {
-        // Generate a random Int32 using arc4Random
         let randomValue = 1 + arc4random() % 6
         return Int(randomValue)
-    }
-    
-    // MARK: Actions
-    @IBAction func rollTheDice(){
     }
 }
