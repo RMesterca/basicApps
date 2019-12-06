e63d3f3387a3d49e83348b0f2eb0e63c6de23a97
rmesterca <mesterca@d-tt.nl>
2019-12-06 23:21:41 +0100

    #time 2m removed unused code

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -28,11 +28,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func didPressPlus(_ sender: Any) {
         changeModes(newMode: .addition)
     }
